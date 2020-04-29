77ac6d1b1d218f0c6117979e90666b95658f3dd7
Helder77270 <34067975+Helder77270@users.noreply.github.com>
2020-04-29 13:29:28 +0100

    Update arbre.py

--- arbre.py
+++ arbre.py
@@ -1,14 +1,17 @@
 class ArbreBinaire:
     value = 0
+    noeudGauche = 0
+    noeudDroit = 0
     def __init__(self):
         self.value = 0
 
-    def getValue():
+    def getValue(self):
         return self.value
         
-    def setValue(val):
+    def setValue(self,val):
         self.value = val
 
 bernard = ArbreBinaire()
 bernard.setValue(1)
 
+print(bernard.getValue())
