5ccbefd5109db46ac9f40e298eb24a212ede2bf3
Helder77270 <34067975+Helder77270@users.noreply.github.com>
2020-04-29 13:19:50 +0100

    Create arbre.py

---
+++ arbre.py
@@ -0,0 +1,14 @@
+class ArbreBinaire:
+    value = 0
+    def __init__(self):
+        self.value = 0
+
+    def getValue():
+        return self.value
+        
+    def setValue(val):
+        self.value = val
+
+bernard = ArbreBinaire()
+bernard.setValue(1)
+
