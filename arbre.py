43d602aa7b52949707a22ac77303ddb5e0175ffe
Helder77270 <34067975+Helder77270@users.noreply.github.com>
2020-04-30 08:36:25 +0100

    Update arbre.py

--- arbre.py
+++ arbre.py
@@ -11,6 +11,12 @@ class TestArbreBinaire:
     def setValue(self,val):
         self.value = val
 
+    def setValueG(self,val):
+        self.noeudGauche = val
+
+    def setValueD(self,val):
+        self.noeudDroit = val
+
     def str(self):
         print("Noeud = " + str(self.value) + " noeud gauche = " + str(self.noeudGauche) + " noeud droit = " + str(self.noeudDroit))
 
@@ -20,8 +26,10 @@ class TestArbreBinaire:
         else:
             print(self.value)
 
-        
+bernard = []        
 bernard = TestArbreBinaire()
 bernard.setValue(1)
 bernard.str()
 bernard.prefixe()
+
+print(bernard)
