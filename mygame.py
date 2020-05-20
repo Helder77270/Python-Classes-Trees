879335f12056c32c636b07ac6d8058c0ca1e6425
Helder77270 <34067975+Helder77270@users.noreply.github.com>
2020-05-20 11:44:18 +0100

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -1,4 +1,8 @@
 from tkinter import *
+from PIL import *
+
+WIDTH = 300
+HEIGHT = 300
 
 fen1 = Tk()
 fen1.title("Push your git !")
@@ -6,10 +10,14 @@ fen1.title("Push your git !")
 photolabel = Label(fen1,text = "HI" )
 namefield = Entry(fen1, justify = CENTER, relief="solid", borderwidth = 2)
 btnjouer = Button(fen1, text ='Jouer', anchor = SW)
+photo = PhotoImage(file='download.png')
+label = Label(fen1, image = photo, width = WIDTH, height = HEIGHT)
 
-
-
+label.pack()
+photolabel.pack()
 namefield.pack()
 btnjouer.pack()
-photolabel.pack()
+
 fen1.mainloop()
+
+
